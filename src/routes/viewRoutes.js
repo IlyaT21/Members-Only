3b0bc28258cf81fd8b9bd8b0f3bd13e6b3f9a3bf
IlyaT21 <ilijatoskovic@gmail.com>
2025-03-29 22:58:08 +0100

    fe changes

--- src/routes/viewRoutes.js
+++ src/routes/viewRoutes.js
@@ -6,4 +6,17 @@ router.get("/", (req, res) => {
   res.render("index", { title: "Home Page" });
 });
 
+// Render Auth Pages
+router.get("/login", (req, res) => {
+  res.render("pages/auth/login", { title: "Login" });
+});
+
+router.get("/register", (req, res) => {
+  res.render("pages/auth/register", { title: "Register" });
+});
+
+router.get("/passcode", (req, res) => {
+  res.render("pages/auth/passcode", { title: "Enter a Passcode" });
+});
+
 module.exports = router;
